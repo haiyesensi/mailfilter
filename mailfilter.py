b1fcd68088328f17c60645e215b27c58321a37b9
Haiyesensi <32067085+Haiyesensi@users.noreply.github.com>
2017-12-29 16:09:42 +0800

    project code

--- mailfilter.py
+++ mailfilter.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Created on 2017-12-18 13:10:34
-# Project: mailfilter
+# Created on 2017-12-21 19:57:48
+# Project: uoj
 
 from pyspider.libs.base_handler import *
 import re
-import pymysql
 
-match_rule = "^[a-zA-Z0-9_-]+((\.[a-zA-Z0-9_-]+)|([a-zA-Z0-9_-]+))@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$"
+
+def validemail(e):
+    if len(e)>= 5:
+        if re.match("^[a-zA-Z0-9_-]+((\.[a-zA-Z0-9_-]+)|([a-zA-Z0-9_-]+))@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$",e) !=None:
+            return e
+    return False
 
 class Handler(BaseHandler):
     crawl_config = {
@@ -28,29 +32,22 @@ class Handler(BaseHandler):
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
         user_url = 'http://uoj.ac/user/profile/'
-        string = response.doc('td > span').text()  
-        list = string.split(" ")
-        for i in list:
-            realuser_url = user_url+i
-            print(realuser_url)            
-            self.crawl(realuser_url, callback=self.getmail_page)
+        usernames = response.doc('td > span').text().split(" ")
+        for username in usernames:
+            url = user_url + username      
+            self.crawl(url, save={'name': username}, callback=self.getmail_page)
 
-#得到每个用户资料页面下的邮箱
+#得到每个用户资料页面下的邮箱,Rating数量,AC数量
     def getmail_page(self,response):
-        usermail = response.doc('body > div > div.uoj-content > div > div.panel-body > div.row > div.col-md-8.col-md-pull-4 > div > div:nth-child(2) > p').text()
-        saveMysql(usermail)
-        
-                
-def saveMysql(str):
-    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='jwerrrer71a', db='spidertest')
-    cursor = conn.cursor()
-    sql = 'INSERT INTO mailfilter(mailaddress,status)VALUES (%s,%s)'
-    if (re.match(match_rule, str) == None):
-        cursor.execute(sql, (str, "n"))
-    else:
-        cursor.execute(sql, (str,""))
-    conn.commit()
-    cursor.close()
-    conn.close()   
-        
-        
+        mail = response.doc('body > div > div.uoj-content > div > div.panel-body > div.row > div.col-md-8.col-md-pull-4 > div > div:nth-child(2) > p').text()
+        rating_count = response.doc('.list-group-item-text > strong').text()
+        AC_count = response.doc('body > div.container.theme-showcase > div.uoj-content > div > div.panel-body > div.list-group > div:nth-child(2) > h4').text()
+        AC_count = re.search('[0-9]+',AC_count).group()
+        #合法邮箱用pyspider自带数据库储存
+        if validemail(mail):
+            return {
+                'name': response.save['name'],
+                'email': mail,
+                'rating': rating_count,
+                'AC_count': AC_count
+            }   
